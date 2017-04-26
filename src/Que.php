6d9c2a712bb3e363fbc5b289655fab562c1b0aea
Jimmie Nilsson <jimmie@asitis.se>
2017-04-26 15:51:32 +0200

    Added initial communication code.

--- src/Que.php
+++ src/Que.php
@@ -15,6 +15,28 @@ class Que
 		
 	}
 
+	public function Connect($address, $port, $timeout)
+	{
+		if(is_numeric($timeout) == false)
+		{
+			$timeout = ini_get("default_socket_timeout");
+		}
+
+		$this->socket = @fsockopen($address, $port, $errno, $errstr, $timeout); 
+
+		if(!$this->socket)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	public function Disconnect()
+	{
+
+	}
+
 	public function IsConnected()
 	{
 		
@@ -22,7 +44,20 @@ class Que
 
 	public function Push($message, $timeout)
 	{
-		
+		if($this->_Send("PUSH " . strlen($message) . "\n" . $message . "\n") != 0)
+		{
+			return false;
+		}
+
+		$recv = $this->_Receive($timeout);
+		if(is_string($recv) == false)
+		{
+			$this->_Send("ABORT\n");
+
+			return false;
+		}
+
+		return $recv;
 	}
 
 	public function Pull($timeout)
@@ -38,6 +73,36 @@ class Que
 	public function Abort()
 	{
 		
+	}
+
+	private function _Send($data)
+	{
+		if(fwrite($this->socket, $data) === false)
+		{
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private function _Receive($timeout)
+	{
+		if(is_integer($timeout))
+		{
+			stream_set_blocking($this->socket, TRUE);
+        	stream_set_timeout($this->socket, $timeout);
+		}
+
+		$recv = fread($this->socket, 2000);
+        $info = stream_get_meta_data($this->socket);
+
+        if ($info['timed_out'])
+        {
+        	echo "Receive timed out!\n";
+        	return false;
+	    }
+	    
+	    return $recv;
 	}
 
 }
---
+++ test/Test.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once("../src/Que.php");
+
+ProducerTest();
+
+/*
+echo 'Type "consumer" or "producer" to start: ';
+
+$test_type_error = false;
+do
+{
+	$test_type = strtoupper(GetInput());
+
+	if($test_type == "CONSUMER")
+	{
+		ConsumerTest();
+	}
+	else if($test_type == "PRODUCER")
+	{
+		ProducerTest();
+	}
+	else
+	{
+		$test_type_error = true;
+		echo "Invalid input, try again: ";
+	}
+} while($test_type_error == true);
+*/
+
+function GetInput()
+{
+	if (PHP_OS == 'WINNT')
+	{
+	  $line = stream_get_line(STDIN, 1024, PHP_EOL);
+	}
+	else
+	{
+	  $line = readline('');
+	}
+
+	return $line;
+}
+
+function ConsumerTest()
+{
+	
+}
+
+function ProducerTest()
+{
+	$que = new Que();
+	if($que->Connect("127.0.0.1", 11400, 3) == false)
+	{
+		echo "Failed to connect to server.\n";
+		die;
+	}
+	
+	echo "Connected!\n";
+
+
+	while(1)
+	{
+		$message = "Hello world!";
+		$timeout = 3;
+
+		echo "Push message: " . $message . "\n";
+		$response = $que->Push($message, $timeout);
+
+		if(is_string($response) == false)
+		{
+			echo "No consumer pulled message.\n";
+			continue;
+		}
+
+		echo "Consumer acked: " . $response . "\n";
+	}
+}
+
+
+
+?>
